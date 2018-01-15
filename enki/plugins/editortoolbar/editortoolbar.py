28482f6c10d025de986985136259474345ed164d
Marco Laspe <marco@rockiger.com>
2018-01-15 10:42:39 +0100

    fixed bug when changing eol

--- enki/plugins/editortoolbar/editortoolbar.py
+++ enki/plugins/editortoolbar/editortoolbar.py
@@ -146,7 +146,7 @@ class EolIndicatorAndSwitcher(QToolButton):
         document = core.workspace().currentDocument()
         document.qutepart.eol = newEol
         document.qutepart.document().setModified(True)
-        self._setEolMode(document.qutepart.eol)
+        self._setEolMode(document.qutepart.eol, newEol)
 
     def _setEolMode(self, document, mode):
         """Change EOL mode on GUI
